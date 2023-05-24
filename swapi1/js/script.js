8b76d7aff3d4095aff450435f3c3548e291aadc2
ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>
2023-05-24 13:06:12 +0200

    nav work

--- swapi1/js/script.js
+++ swapi1/js/script.js
@@ -1,11 +1,39 @@
 const baseurl = "https://swapi.dev/api"
 
-async function getData(){
+const navBar = document.querySelector(".nav-bar")
+
+fetch(baseurl)
+    .then(res => {
+        return res.json()
+    })
+    .then(menuData => {
+        for(let menuItem in menuData){
+            let navItem = document.createElement("a");
+            navItem.addEventListener("click", navClick);
+            navItem.className = "nav-item";
+            navItem.innerText = menuItem;
+            navItem.href = menuData[menuItem];
+            navBar.appendChild(navItem);
+        }
+})
+
+async function navClick(e){
+    e.preventDefault();
+    const data = await getData(this.href);
+    console.log(data);
+    // alert(this.href);
+}
+
+async function getData(url){
     try {
-        const res = await fetch(baseurl)
+        const res = await fetch(url)
         return await res.json()
     }
     catch(err){
         console.log(err)
     }
 }
+
+
+
+// async getData().then(data => console.log(data))
