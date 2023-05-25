fa9e6d54eb1c14e0603c88ce1a57c1123fa5a368
ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>
2023-05-25 13:25:06 +0200

    button wip

--- SwapiRepete/js/script.js
+++ SwapiRepete/js/script.js
@@ -1,9 +1,12 @@
 
 const swapiApp = (async function() 
 {
-    const SWApIURL = "https://swapi.dev/api"
-    const navBar = document.querySelector("#nav-bar")
-    const cardContrainer = document.querySelector(".card-container")
+    const SWApIURL = "https://swapi.dev/api";
+    const navBar = document.querySelector("#nav-bar");
+    const cardContrainer = document.querySelector(".card-container");
+    const buttonNextPrec = document.querySelector(".button-next-prev");
+    let next = "";
+    let previous = "";
 
     try 
     {
@@ -18,6 +21,8 @@ const swapiApp = (async function()
             navItem.href = jsonData[key];
             navBar.appendChild(navItem);
         }
+        
+        
     }
     catch (error) 
     {
@@ -27,19 +32,24 @@ const swapiApp = (async function()
     async function navClick(e)
     {
         e.preventDefault();
+        deleteNavButton();
+        // cardContrainer.innerHTML = "";
+        document.querySelector(".active")?.classList.remove("active");
+        this.classList.add("active");
         let data = await getData(this.href);
+        navButton();
+        deleteCard();
         data.results.forEach(dataItem =>{
             let card = document.createElement("div");
             card.className = "card";
-            // card.innerText = dataItem.name;
+            next = data.next;
+            previous = data.previous;
             for(let [k, v] of Object.entries(dataItem))
             {
-                card.insertAdjacentHTML("beforeend", `<span class="key">${k}:</span> <span class="value">${v}</span><br>`)
+                card.insertAdjacentHTML("beforeend", `<span class="key">${k}:</span> <span class="value">${v}</span><br><hr>`)
             }
             cardContrainer.appendChild(card);
-            // cardContrainer.appendChild(card);
         })
-        // console.log(data);
     }
 
     async function getData(url)
@@ -52,6 +62,35 @@ const swapiApp = (async function()
         catch(error)
         {
             console.log(error);
+        }
+    }
+
+    function deleteCard(){
+        while(cardContrainer.firstChild)
+        {
+            cardContrainer.removeChild(cardContrainer.firstChild);
+        }
+    }
+
+    function navButton(){
+        let nextButton = document.createElement("button");
+        let prevButton = document.createElement("button");
+        nextButton.className = "button-next-prev";
+        prevButton.className = "button-next-prev";
+        nextButton.innerText = "Next";
+        prevButton.innerText = "Prev";
+        nextButton.href = next;
+        prevButton.href = previous;
+        buttonNextPrec.appendChild(prevButton);
+        buttonNextPrec.appendChild(nextButton);
+        nextButton.addEventListener("click", navClick);
+        prevButton.addEventListener("click", navClick);
+    }
+
+    function deleteNavButton(){
+        while(buttonNextPrec.firstChild)
+        {
+            buttonNextPrec.removeChild(buttonNextPrec.firstChild);
         }
     }
 
